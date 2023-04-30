9f91add5c993adc858ca5871df7acb44a6828379
Iman Hamza <iman.ali.hamza@gmail.com>
2023-04-29 20:53:11 -0700

    102-zombie.c created

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int infinite_while(void);
+
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+	pid_t pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid == 0)
+			exit(0);
+		else
+			printf("Zombie process created, PID: %d\n", pid);
+	}
+	infinite_while();
+}
+
+/**
+ * infinite_while - runs forever
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
