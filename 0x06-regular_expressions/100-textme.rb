4350ae7eaf4700bd3df1f8174fe0947a3ba5e5c3
Iman Hamza <iman.ali.hamza@gmail.com>
2023-05-02 14:04:43 -0700

    100-textme.rb updated

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,9 +2,9 @@
 string = ARGV[0]
 sender = string.scan(/from:([a-zA-Z]{1,}|.[0-9]{1,})/)
 rec = string.scan(/to:(.[0-9]{1,}|.[a-zA-Z]{1,})/)
-flags = string.scan(/flags:(-[0-9]:[0-9]:-[0-9]:[0-9]:-[0-9])/)
+flags = string.scan(/flags:(-\d|\d):(-\d|\d):(-\d|\d):(-\d|\d):(-\d|\d)/)
 print sender.join
 print ","
 print rec.join
 print ","
-puts flags.join
+puts flags.join(':')
