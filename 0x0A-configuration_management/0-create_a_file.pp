db4d63beb150e3e254ffd35f6c6b602b2c551ceb
Iman Hamza <iman.ali.hamza@gmail.com>
2023-06-23 16:26:41 -0700

    update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 file { '/tmp/school':
-ensure  => file,
-content => 'I love Puppet',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  path    => '/tmp/school'
 }
